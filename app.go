72147fdba20d0f708d5cf5338f2a18dbd38df643
SimonIsMe <szym1991@gmail.com>
2017-10-21 19:33:38 +0200

    Create init/build app files

---
+++ app.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/AwesomeTeamPlayer/auth-service-go/server"
+	"fmt"
+)
+func main() {
+	fmt.Println("Start")
+	server.StartServer()
+}
---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+go get github.com/streadway/amqp
+
+go get github.com/go-sql-driver/mysql
+go get github.com/gorilla/mux
+go get github.com/gorilla/rpc
+go get github.com/gorilla/rpc/json
